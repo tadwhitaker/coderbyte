85996a587023c8c73c5f385b64c2aa9902d4e911
Tad <tad@whitakerresearch.com>
2015-03-27 14:51:12 -0700

    Adding new vowel_count code

--- vowel_count.js
+++ vowel_count.js
@@ -1,32 +1,12 @@
 var str = "Argument goes here"
 
 function VowelCount(str) {
-	var bozo = str.toLowerCase();
-	console.log(bozo);
-	var total = []; 
-	for (var i = 0; i <= bozo.length; i++) {
-		if (bozo[i] == "a") {
-			total.push(bozo[i]);
-		}
-		else if (bozo[i] == "e") {
-			total.push(bozo[i]);
-		}
-		else if (bozo[i] == "i") {
-			total.push(bozo[i]);
-		}
-		else if (bozo[i] == "o") {
-			total.push(bozo[i]);
-		}
-		else if (bozo[i] == "u") {
-			total.push(bozo[i]);
-		}
-		else {
-			return false;
-		}
+	var total = 0
+	str = str.toLowerCase().replace(/[^aeiou]/g, "");
+	total = str.length;
 	console.log(total);
-	// 	var bike = total.length;
-	// 	console.log(bike);
-	}
 };
 
 VowelCount(str);
+
+
